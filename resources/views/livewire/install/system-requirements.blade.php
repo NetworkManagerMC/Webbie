c6f2ae4f18ba2c723104c1d2f7619b5a5be2592d
Aaron Belz <belzaaron@gmail.com>
2021-10-20 13:01:25 -0400

    Pretty print the system requirements

--- resources/views/livewire/install/system-requirements.blade.php
+++ resources/views/livewire/install/system-requirements.blade.php
@@ -1,5 +1,28 @@
 <div>
-    System Requirements
+    <h6>System Requirements</h6>
     <hr />
-    {{ json_encode($systemRequirements) }}
+    <ul class="list-group">
+        @php
+            $listGroupItemBaseClasses = 'list-group-item d-flex justify-content-between align-items-center';
+        @endphp
+
+        <li @class([$listGroupItemBaseClasses, 'list-group-item-success'])>
+            PHP - version
+            <span class="badge bg-primary rounded-pill">{{ $systemRequirements['php']['current'] }}</span>
+        </li>
+
+        @foreach ($systemRequirements['modules']['requirements'] as $module => $requirements)
+            @foreach ($requirements as $name => $value)
+                <li @class([
+                    $listGroupItemBaseClasses,
+                    'list-group-item-success' => $value,
+                    'list-group-item-error' => ! $value,
+                ])>
+                    {{ strtoupper($module) }} - {{ $name }}
+                    <span class="badge bg-primary rounded-pill">{{ $value ? 'Yes' : 'No' }}</span>
+                </li>
+            @endforeach
+        @endforeach
+
+    </ul>
 </div>
