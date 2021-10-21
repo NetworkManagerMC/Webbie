d8c3384a15b70c6d0fe820641d83d7b38e50e833
Aaron Belz <belzaaron@gmail.com>
2021-10-20 23:17:17 -0400

    Review page polish

--- resources/lang/en/install.php
+++ resources/lang/en/install.php
@@ -12,4 +12,9 @@ return [
         'could-not-connect' => 'There is an error with the connection credentials entered.',
     ],
 
+    'installation-review' => [
+        'summary' => 'The Webbie application is nearly installed. In order to complete the installation process you must press the finish button from below. This will write the connection details to the private ".env" file and create an administrator account for NetworkManager using the credentials you\'ve entered.',
+        'alert' => 'Once the system is finished installing, you will gain the full power and convenience of NetworkManager\'s Webbie.',
+    ],
+
 ];
--- resources/views/livewire/install/installation-review.blade.php
+++ resources/views/livewire/install/installation-review.blade.php
@@ -1,3 +1,9 @@
 <div>
-    Installation Review
+    <h6>Installation Review</h6>
+
+    <hr />
+
+    <p>{{ __('install.installation-review.summary') }}</p>
+
+    <div class="alert alert-primary mb-0">{{ __('install.installation-review.alert') }}</div>
 </div>
